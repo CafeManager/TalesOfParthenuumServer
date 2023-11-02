ded0ed46708ff112c9176f1a0d47e9c6f33c1742
CafeManager <kevinflores1997@yahoo.com>
2023-11-02 00:36:20 -0700

    changed port to 8020

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const uuidv4 = require("uuid").v4;
 
 // Spinning the http server and the WebSocket server.
 const server = http.createServer();
-const wsServer = new WebSocketServer({ port: 8000 });
+const wsServer = new WebSocketServer({ port: 8020 });
 const port = 8000;
 server.listen(port, () => {
     console.log(`WebSocket server is running on port ${port}`);

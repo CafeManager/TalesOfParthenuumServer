e56291624c7cb116e7cb62dac889b4d98f12f056
CafeManager <kevinflores1997@yahoo.com>
2023-11-02 00:32:17 -0700

    fixed port bug

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const uuidv4 = require("uuid").v4;
 
 // Spinning the http server and the WebSocket server.
 const server = http.createServer();
-
+const wsServer = new WebSocketServer({ port: 8000 });
 const port = 8000;
-const wsServer = new WebSocketServer({ server, port: 8000 });
 server.listen(port, () => {
     console.log(`WebSocket server is running on port ${port}`);
 });
